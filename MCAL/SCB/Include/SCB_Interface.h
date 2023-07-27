78e25917fd65e36b7c48b720eef5abc580261ced
m7mdwalid1 <65432868+m7mdwalid1@users.noreply.github.com>
2023-07-27 15:23:09 +0300

    adding new function to SCB

--- MCAL/SCB/Include/SCB_Interface.h
+++ MCAL/SCB/Include/SCB_Interface.h
@@ -24,6 +24,23 @@ typedef enum
     GP_2_SP_8 = 0x05FA0600UL,  // 2 Group of Priority & 8 Sub Group of Priority
     GP_0_SP_16 = 0x05FA0700UL, // 0 Group of Priority & 16 Sub Group of Priority
 } PRIGROUP_t;
+typedef enum
+{
+    MEMFAULT = 16,
+    BUSFAULT,
+    USAGEFAULT
+} Fault_t;
+typedef enum
+{
+    NMI_HANDLER = 0,
+    HARDFAULT_HANDLER,
+    MEMMANAGE_HANDLER,
+    BUSFAULT_HANDLER,
+    USAGEFAULT_HANDLER,
+    SVCALL_HANDLER,
+    DEBUGMONITOR_HANDLER,
+    PENDSV_HANDLER
+} HANDLER_t;
 /*==============================================================================================================================================
  *@fn SCB_VoidSetPriorityGroup
  *@brief Setting Priority Group
@@ -31,5 +48,18 @@ typedef enum
  *@retval void
  *==============================================================================================================================================*/
 void SCB_VoidSetPriorityGroup(PRIGROUP_t Local_u8PriorityGroup);
-
+/*==============================================================================================================================================
+ *@fn SCB_VoidEnableFaultException
+ *@brief Enable Fault Exception
+ *@paramter[in] Local_Fault: Fault Type
+ *@retval void
+ *==============================================================================================================================================*/
+void SCB_VoidEnableFaultException(Fault_t Local_Fault);
+/*==============================================================================================================================================
+ *@fn SCB_VoidDisableFaultException
+ *@brief Disable Fault Exception
+ *@paramter[in] Local_Fault: Fault Type
+ *@retval void
+ *==============================================================================================================================================*/
+void SCB_VoidDisableFaultException(Fault_t Local_Fault);
 #endif /* SCB_INCLUDE_SCB_INTERFACE_H_ */
--- MCAL/SCB/SRC/SCB_Program.c
+++ MCAL/SCB/SRC/SCB_Program.c
@@ -16,8 +16,18 @@
  *******************************************************************************/
 #include <stdint.h>
 #include "../../../LIB/STM32F446.h"
+#include "../../../LIB/ErrorType.h"
 #include "../Include/SCB_Interface.h"
 #include "../Include/SCB_Private.h"
+/*==============================================================================================================================================
+ * GLOBAL VARIABLES SECTION
+ *==============================================================================================================================================*/
+
+static void (*SCB_PTR_TO_FUNCTION[8])(void) = {NULL};
+
+/*==============================================================================================================================================
+ * FUNCTION DEFINITION SECTION
+ *==============================================================================================================================================*/
 /*==============================================================================================================================================
  *@fn SCB_VoidSetPriorityGroup
  *@brief Setting Priority Group
@@ -28,4 +38,108 @@ void SCB_VoidSetPriorityGroup(PRIGROUP_t Local_u8PriorityGroup)
 {
     /*Setting Priority Group*/
     SCB->AIRCR = Local_u8PriorityGroup;
+}
+/*==============================================================================================================================================
+ *@fn SCB_VoidEnableFaultException
+ *@brief Enable Fault Exception
+ *@paramter[in] Local_Fault: Fault Type
+ *@retval void
+ *==============================================================================================================================================*/
+void SCB_VoidEnableFaultException(Fault_t Local_Fault)
+{
+    /*setting Enable Bit For Fault Exception*/
+    SCB->SHCSR |= (1 << Local_Fault);
+}
+/*==============================================================================================================================================
+ *@fn SCB_VoidDisableFaultException
+ *@brief Disable Fault Exception
+ *@paramter[in] Local_Fault: Fault Type
+ *@retval void
+ *==============================================================================================================================================*/
+void SCB_VoidDisableFaultException(Fault_t Local_Fault)
+{
+    /*Clearing Enable Bit For Fault Exception*/
+    SCB->SHCSR &= ~(1 << Local_Fault);
+}
+
+uint8_t SCB_u8SetHandler(HANDLER_t Local_Handler, void (*Local_ptrToFunction)(void))
+{
+    uint8_t Local_u8ErrorState = OK;
+    if (Local_Handler < PENDSV_HANDLER && Local_ptrToFunction != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[Local_Handler] = Local_ptrToFunction;
+    }
+    else
+    {
+        Local_u8ErrorState = NOK;
+    }
+    return Local_u8ErrorState;
+}
+
+/*==============================================================================================================================================
+ * HANDLERS SECTION
+ *==============================================================================================================================================*/
+
+void NMI_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[NMI_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[NMI_HANDLER]();
+    }
+}
+
+void HardFault_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[HARDFAULT_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[HARDFAULT_HANDLER]();
+    }
+}
+
+void MemManage_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[MEMMANAGE_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[MEMMANAGE_HANDLER]();
+    }
+}
+
+void BusFault_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[BUSFAULT_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[BUSFAULT_HANDLER]();
+    }
+}
+
+void UsageFault_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[USAGEFAULT_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[USAGEFAULT_HANDLER]();
+    }
+}
+
+void SVC_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[SVCALL_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[SVCALL_HANDLER]();
+    }
+}
+
+void DebugMon_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[DEBUGMONITOR_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[DEBUGMONITOR_HANDLER]();
+    }
+}
+
+void PendSV_Handler(void)
+{
+    if (SCB_PTR_TO_FUNCTION[PENDSV_HANDLER] != NULL)
+    {
+        SCB_PTR_TO_FUNCTION[PENDSV_HANDLER]();
+    }
 }
